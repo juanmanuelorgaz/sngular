bfd3d7b6536225adfc0f8adfcd0d5825898016a2
juanmanuelorgaz <50863121+juanmanuelorgaz@users.noreply.github.com>
2020-04-20 17:24:31 +0200

    Update scriptmac.sh

--- scriptmac.sh
+++ scriptmac.sh
@@ -1,1 +1,23 @@
-mdfind -onlyin "/Applications" -name "GoTo" | xargs rm -rf
+mdfind -onlyin "/Applications" -name "Vuze" | xargs rm -rf
+mdfind -onlyin "/Applications" -name "BitComet" | xargs rm -rf
+mdfind -onlyin "/Applications" -name "eMule" | xargs rm -rf
+mdfind -onlyin "/Applications" -name "Deluge" | xargs rm -rf
+mdfind -onlyin "/Applications" -name "TotalD" | xargs rm -rf
+mdfind -onlyin "/Applications" -name "Tixati" | xargs rm -rf
+mdfind -onlyin "/Applications" -name "Tribler" | xargs rm -rf
+mdfind -onlyin "/Applications" -name "BitLord" | xargs rm -rf
+mdfind -onlyin "/Applications" -name "FrostWire" | xargs rm -rf
+mdfind -onlyin "/Applications" -name "BiglyBT" | xargs rm -rf
+mdfind -onlyin "/Applications" -name "Bitche" | xargs rm -rf
+mdfind -onlyin "/Applications" -name "Soulseek" | xargs rm -rf
+mdfind -onlyin "/Applications" -name "Shareaza" | xargs rm -rf
+mdfind -onlyin "/Applications" -name "KCeasy" | xargs rm -rf
+mdfind -onlyin "/Applications" -name "Ares Galaxy" | xargs rm -rf
+mdfind -onlyin "/Applications" -name "WinMX" | xargs rm -rf
+mdfind -onlyin "/Applications" -name "WireShare" | xargs rm -rf
+mdfind -onlyin "/Applications" -name "Gnutella" | xargs rm -rf
+mdfind -onlyin "/Applications" -name "Limeware" | xargs rm -rf
+mdfind -onlyin "/Applications" -name "Kazaa" | xargs rm -rf
+mdfind -onlyin "/Applications" -name "iMesh" | xargs rm -rf
+mdfind -onlyin "/Applications" -name "uTorrent" | xargs rm -rf
+mdfind -onlyin "/Applications" -name "BitTorrent" | xargs rm -rf
