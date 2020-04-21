a761b0faa0271b28f7f3f450f7e5200481b42c4c
juanmanuelorgaz <50863121+juanmanuelorgaz@users.noreply.github.com>
2020-04-21 13:06:51 +0200

    Update scriptwin.bat

--- scriptwin.bat
+++ scriptwin.bat
@@ -1,3 +1,4 @@
 @echo off
-wmic product where name="GoTo opener" call uninstall /nointeractive >> logwmic.txt
-echo "Fin"
+wmic product where name="GoTo opener" call uninstall /nointeractive >> C:\WINDOWS\Temp\logscriptwin.txt
+del %ProgramData%\Microsoft\Windows\Start Menu\Programs\StartUp\scriptwin.bat
+pause
